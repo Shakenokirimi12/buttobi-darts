717b666cc670a759d8b5b9b188c085c8ea26450c
shakenokirimi12 <ken20051205@gmail.com>
2024-10-12 06:28:36 +0900

    Delete react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navi from "./Components/Navi.jsx";
-import Items from "./Components/Items.jsx";
 import "./App.css";
 import Start from "./Components/Start.jsx";
 
 function App() {
   return (
     <ChakraProvider>
-      <Router>
-        <Routes>
-          <Route path="/getNavigation" element={<Navi />} />
-          <Route path="/getItems" element={<Items />} />
-          <Route path="/startNavigation" element={<Start />} />
-        </Routes>
-      </Router>
+      <Start />
     </ChakraProvider>
   );
 }
